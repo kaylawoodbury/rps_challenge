04b1c1d7de206d1dea3716a905e018ec03f01fb8
kaylawoodbury <kaylasandoval92@gmail.com>
2020-03-05 20:01:02 +0100

    fix logic for game, update styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,20 @@ import {computerSelection, game, gameTally } from './modules/gameLogic'
 
 class App extends Component {
 	state = {
-		playerChoice: '',
-		computerChoice: '',
-    results: false,
+		player: '',
+		computer: '',
+    results: '',
     playerTally: 0,
-    computerTally: 0
+    computerTally: 0,
+    tally: null,
+    showMessage: false
   }
   
-  async handleButtonClick(event) {
+ handleButtonClick(event) {
 		let player = event.target.name
     let computer = computerSelection()
-    let results = await game(player, computer);
-		this.setState({ playerChoice: player, computerChoice: computer, results: results})
+    let tally = gameTally()
+		this.setState({ player: player, computer: computer, results: game(player, computer), showMessage: true})
 	}
 
   render() {
@@ -30,25 +32,28 @@ class App extends Component {
           <div id="rockcontainer">
             <h3 id="rock">Rock</h3>
             <img src={rock} id="rockimg"/>
-            <button id="rockbutton" onClick={event => this.handleButtonClick(event)}>Play w/ Rock</button>
+            <button id="rockbutton" onClick={event => this.handleButtonClick(event)} name="rock">Play w/ Rock</button>
           </div>
           <div id="papercontainer">
             <h3 id="paper">Paper</h3>
             <img src={paper} id="paperimg"/>
-            <button id="paperbutton" onClick={event => this.handleButtonClick(event)}>Play w/ Paper</button>
+            <button id="paperbutton" onClick={event => this.handleButtonClick(event)} name="paper">Play w/ Paper</button>
           </div>
           <div id="scissorscontainer">
             <h3 id="scissors">Scissors</h3>
             <img src={scissors} id="scissorsimg"/>
-            <button id="scissorbutton" onClick={event => this.handleButtonClick(event)}>Play w/ Scissors</button>
+            <button id="scissorbutton" onClick={event => this.handleButtonClick(event)} name="scissors">Play w/ Scissors</button>
           </div>
-        </div><br/><br/><br/>
-        <div id="results">
-          <h2>Result:</h2><br/>
-          <h3>Your choice:  Computer Choice:</h3>
-          <h3>{this.state.results}</h3>
-          
         </div>
+        {this.state.showMessage &&
+        <>
+        <div id="results">
+        <h3>{`Your choice: ${this.state.player}`}<br/>{`Computer Choice: ${ this.state.computer}`}</h3><br/>
+          <h2>Result:</h2>
+          <h3>{this.state.results}</h3>
+          </div>
+        </>
+        }
       </div>
     );
   }
